43489a022df5db33b12e4c458ff77a236483c5e0
Bachtran301 <bachtranduy2003@gmail.com>
2024-11-10 22:47:40 +0700

    change log

---
+++ src/header.js
@@ -0,0 +1,16 @@
+const headers = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en",
+    "Content-Type": "multipart/form-data",
+    "Origin": "https://app.bums.bot",
+    "Referer": "https://app.bums.bot/",
+    "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+    "Sec-Ch-Ua-Mobile": "?1",
+    "Sec-Ch-Ua-Platform": '"Android"',
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"
+};
+module.exports = headers;
